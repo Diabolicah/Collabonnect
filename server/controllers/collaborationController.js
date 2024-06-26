2590258b1f2dd94173719103556b4f066f2f0252
Diabolica Elias <scripter.diabolica@gmail.com>
2024-06-26 15:21:18 +0300

    Fixed mysql to mysql2 and added getCollaboration to controller

--- server/controllers/collaborationController.js
+++ server/controllers/collaborationController.js
@@ -1,9 +1,14 @@
 const {dbConnection} = require('../db_connection');
 
+const TABLE_NAME_PREFIX = "tbl_112"
+
 const collaborationController = {
     // GET /api/collaboration
     async getCollaborations(req, res) {
-
+        const connection = await dbConnection.createConnection();
+        const [collaborations] = await connection.execute(`SELECT * FROM ${TABLE_NAME_PREFIX}_collaboration`);
+        connection.end();
+        res.json(collaborations);
     },
     // GET /api/collaboration/:id
     async getCollaborationById(req, res) {},
@@ -14,3 +19,5 @@ const collaborationController = {
     // DELETE /api/collaboration/:id
     async deleteCollaboration(req, res) {}
 };
+
+module.exports = { collaborationController };

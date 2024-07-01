5146d182e47a726ca7e8eb09feb2090502e460f7
julio1703 <89707649+julio1703@users.noreply.github.com>
2024-07-02 00:03:38 +0300

    fixed the threshold progress bar to work dynamically

--- client/js/objectDetailsScripts.js
+++ client/js/objectDetailsScripts.js
@@ -1,27 +1,36 @@
-let user_id, domain;
+function updateThresholdProgressBar(progressBar, threshold) {
+    const progress_color = threshold < 25 ? "#DC3545" : threshold < 50 ? "#FD7E14" : threshold < 75 ? "#FFC107" : "#20C997"
+    progressBar.textContent = `${threshold}%`
+    progressBar.style.width = `${threshold}%`;
+    progressBar.style.backgroundColor = progress_color;
+}
 
+async function initObjectDetails(objectData){
+    const {brandLogo, developerLogo} = await getCollaborationLogos(objectData);
+    const threshold = await getCollaborationThresholdPercentage(objectData);
+    document.querySelector("#collaboration_title").textContent = objectData.title;
+    document.querySelector(".breadcrumb-item.active").textContent = objectData.title;
+    document.querySelector("#collaboration_status").textContent = `Status: ${objectData.status}`;
+    document.querySelectorAll("#collaboration_upvotes_downvotes span")[0].textContent = objectData.upvote;
+    document.querySelectorAll("#collaboration_upvotes_downvotes span")[1].textContent = objectData.downvote;
+    document.querySelector(".circular_progress_bar span").textContent = objectData.ai_readability;
+    updateCircularProgressBar(document.querySelector(".circular_progress_bar"), objectData.ai_readability, "#2E2C2C");
+    document.querySelector("#collaboration_brand_logo").src = `${brandLogo}`;
+    document.querySelector("#collaboration_developer_logo").src = `${developerLogo}`;
+    updateThresholdProgressBar(document.querySelector("#container_object_details .progress .progress-bar"), threshold);
+    console.log(await getCollaborationCoWritersProfileImages(objectData));
 
+}
 
 async function getObjectDetailsFromServer(){
     const urlParams = new URLSearchParams(window.location.search);
     const collaborationId = urlParams.get("id");
-    await fetch(`${domain}/api/collaboration/${collaborationId}`)
-        .then(response => {
-            if(response.status == 200)
-                return response.json();
-            else if(response.status == 404){
-                window.location.replace("./index.html");
-            }
-        })
-        .then(initObjectDetails);
+    const collaboration = await getCollaborationDetails(collaborationId);
+    if(collaboration)
+        initObjectDetails(collaboration);
+    else document.location.replace("./index.html");
 }
 
 window.onload = async () => {
-    await fetch("./data/settings.json")
-        .then((response) => response.json())
-        .then(data => {
-            user_id = data.user_id;
-            domain = data.domain;
-        });
     getObjectDetailsFromServer();
 }

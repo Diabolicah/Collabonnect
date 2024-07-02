7f553ae0c11ebe3c9cf82e0a78a2c443c10d047f
Diabolica Elias <scripter.diabolica@gmail.com>
2024-07-02 04:20:26 +0300

    Started the base of create new collaboration

--- client/js/homepageScripts.js
+++ client/js/homepageScripts.js
@@ -1,3 +1,29 @@
+async function populateFormBrandSelection() {
+    const { domain } = await fetch("./data/settings.json").then((response) => response.json());
+    const brands = await fetch(`${domain}/api/brand/`).then(response => response.json());
+
+    const brandSelect = document.getElementById("collaborationBrandDataList");
+    brands.forEach(brand => {
+        const option = document.createElement("option");
+        option.value = brand.id;
+        option.text = brand.name;
+        brandSelect.appendChild(option);
+    });
+}
+
+async function populateFormDeveloperSelection() {
+    const { domain } = await fetch("./data/settings.json").then((response) => response.json());
+    const developers = await fetch(`${domain}/api/developer/`).then(response => response.json());
+
+    const developerSelect = document.getElementById("collaborationDeveloperDatalistOptions");
+    developers.forEach(developer => {
+        const option = document.createElement("option");
+        option.value = developer.id;
+        option.text = developer.name;
+        developerSelect.appendChild(option);
+    });
+}
+
 window.onload = async () => {
     const collaborations = await getCollaborationsList();
     collaborations.forEach(async (collaboration) => {
@@ -5,5 +31,14 @@ window.onload = async () => {
             .then(collaboration_card => {
                 document.getElementById("collaboration_cards_container").appendChild(collaboration_card);
             });
+    });
+
+    populateFormBrandSelection();
+    populateFormDeveloperSelection();
+
+    const object_adder = document.getElementById("object_adder");
+    object_adder.addEventListener("click", () => {
+        const newCollaborationModal = new bootstrap.Modal('#newCollaborationModal', {})
+        newCollaborationModal.show();
     });
 }
